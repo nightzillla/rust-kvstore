bc0083b433fb71fb703fd9a6f348095754c8228c
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-11-06 17:32:09 -0600

    worked on system output

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,14 @@ use std::cmp::Ordering;
 
 fn main() {
     println!("What is your name?");
-    let mut 
-    println!("Hello, world!");
+    let mut name = String::new();
+    let greeting: &str = "Nice to meet you";
+    // to receive input from the user you're going to need to use readline
+    io::stdin().read_line(&mut name)
+        .expect("Didn't Receive Input");
+    println!("Hello {}! {}", name.trim_end(), greeting);
 }
+
+
 
  
